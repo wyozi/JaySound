f1c41db548928b4f6a084cdbd07b4de04f24deea
wyozi <joonasmertanen@gmail.com>
2015-11-30 20:54:22 +0200

    Refactor DRY code from mp3/ogg decoders

--- src/main/java/com/wyozi/jaysound/decoder/Decoder.java
+++ src/main/java/com/wyozi/jaysound/decoder/Decoder.java
@@ -1,7 +1,12 @@
 package com.wyozi.jaysound.decoder;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author Wyozi
@@ -9,4 +14,33 @@ import java.io.IOException;
  */
 public abstract class Decoder {
     public abstract void readFully(DecoderCallback callback) throws IOException;
+
+    protected void readStreamFully(InputStream in, DecoderCallback callback) throws IOException {
+        AudioInputStream audioIn = null;
+        try {
+            audioIn = AudioSystem.getAudioInputStream(new BufferedInputStream(in));
+        } catch (UnsupportedAudioFileException e) {
+            throw new IOException(e);
+        }
+        AudioFormat baseFormat = audioIn.getFormat();
+
+        callback.inform(baseFormat.getChannels(), (int) baseFormat.getSampleRate());
+
+        AudioFormat decodedFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+                baseFormat.getSampleRate(),
+                16,
+                baseFormat.getChannels(),
+                baseFormat.getChannels() * 2,
+                baseFormat.getSampleRate(),
+                false);
+
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(decodedFormat, audioIn);
+        byte[] buffer = new byte[4096];
+        int read;
+        while ((read = audioInputStream.read(buffer, 0, buffer.length)) != -1) {
+            callback.writePCM(buffer, 0, read);
+        }
+
+        audioInputStream.close();
+    }
 }
--- src/main/java/com/wyozi/jaysound/decoder/MP3Decoder.java
+++ src/main/java/com/wyozi/jaysound/decoder/MP3Decoder.java
@@ -18,31 +18,6 @@ public class MP3Decoder extends Decoder {
 
     @Override
     public void readFully(DecoderCallback callback) throws IOException {
-        AudioInputStream audioIn = null;
-        try {
-            audioIn = AudioSystem.getAudioInputStream(new BufferedInputStream(this.in));
-        } catch (UnsupportedAudioFileException e) {
-            throw new IOException(e);
-        }
-        AudioFormat baseFormat = audioIn.getFormat();
-
-        callback.inform(baseFormat.getChannels(), (int) baseFormat.getSampleRate());
-
-        AudioFormat decodedFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
-                baseFormat.getSampleRate(),
-                16,
-                baseFormat.getChannels(),
-                baseFormat.getChannels() * 2,
-                baseFormat.getSampleRate(),
-                false);
-
-        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(decodedFormat, audioIn);
-        byte[] buffer = new byte[4096];
-        int read;
-        while ((read = audioInputStream.read(buffer, 0, buffer.length)) != -1) {
-            callback.writePCM(buffer, 0, read);
-        }
-
-        audioInputStream.close();
+        readStreamFully(in, callback);
     }
 }
